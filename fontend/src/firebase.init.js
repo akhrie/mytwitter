b3535283df9a44480f3e4d62baf3943f9036280a
Khruohinuo Kera <72345600+akhrie@users.noreply.github.com>
2024-01-12 20:58:27 +0530

    Update firebase.init.js

--- fontend/src/firebase.init.js
+++ fontend/src/firebase.init.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAl5-l6Rqc84nDSS_wYep6Do4YDBpzIE_4",
-  authDomain: "fir-app-bbfbd.firebaseapp.com",
-  projectId: "fir-app-bbfbd",
-  storageBucket: "fir-app-bbfbd.appspot.com",
-  messagingSenderId: "1027510644715",
-  appId: "1:1027510644715:web:905907681bfaecc756b3ae",
-  measurementId: "G-G2L4MVHWM1"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
